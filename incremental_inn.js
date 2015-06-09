66a72d4aad6e49eeb5c2f71c24809a182b07537c
Andreas Zwinkau <qznc@web.de>
2015-06-09 22:09:52 +0200

    counting up and preserving state

---
+++ incremental_inn.js
@@ -0,0 +1,40 @@
+(function() {
+	const ID_GAMESTATE = "gstate";
+	const ADD_PER_SECOND = 3;
+	var STATE = Object();
+
+	var lastStepTime = Date.now();
+
+	function oneStep() {
+		var now = Date.now();
+		var elapsedTime_ms = Date.now() - lastStepTime;
+		lastStepTime = now;
+		//console.log("step for "+elapsedTime_ms+"ms with counter = "+STATE.counter);
+
+		STATE.counter += ADD_PER_SECOND * elapsedTime_ms / 1000;
+		var sc = document.getElementById("counter");
+		sc.innerHTML = STATE.counter | 0;
+	}
+
+	function startGame() {
+		var s = Save.load(ID_GAMESTATE) || {};
+		setIfMissing(s, "counter", 0);
+		STATE = s;
+		setInterval(oneStep, 1000);
+		setInterval(saveGame, 10*1000);
+		oneStep();
+	}
+
+	function saveGame() {
+		Save.save(ID_GAMESTATE, STATE);
+		//console.log("saved");
+	}
+
+	function setIfMissing(obj, key, val) {
+		if (key in obj && obj[key] !== undefined && obj[key] !== null) return;
+		//console.log("set "+key+" to "+val);
+		obj[key] = val;
+	}
+
+	startGame();
+})();
