e1ec60e29a90a38a4c679adadb601607de5d6383
Andreas Zwinkau <qznc@web.de>
2015-06-10 22:52:22 +0200

    use deterministic RNG

--- incremental_inn.js
+++ incremental_inn.js
@@ -18,6 +18,7 @@
 		var s = Save.load(ID_GAMESTATE) || {};
 		setIfMissing(s, "counter", 0);
 		setIfMissing(s, "lastStepTime", Date.now());
+		setIfMissing(s, "seed_rng", new RNG(123));
 		STATE = s;
 		setInterval(oneStep, 1000);
 		setInterval(saveGame, 10*1000);
@@ -55,29 +56,8 @@
 		return n;
 	}
 
-	function randRange(min,max) {
-		var range = max-min;
-		assert (range >= 0);
-		return min + (Math.random() * range);
-	}
-
-	function randIndex(list) {
-		return (Math.random() * (list.length-1)).toFixed(0);
-	}
-
-	function choose(items) {
-		var index = (Math.random() * (items.length-1)).toFixed(0);
-		return items[index];
-	}
-
-	function randPop(list) {
-		var idx = randIndex(list);
-		var item = list[idx];
-		list.splice(idx,1);
-		return item;
-	}
-
 	function randInnName() {
+		var rng = STATE.seed_rng;
 		var adj = ["Prancing", "Dancing", "Drinking", "Jolly",
 			"Damned", "Dead", "Lost",
 			"Cold", "Hot",
@@ -89,9 +69,37 @@
 			"Lantern",
 			"Wizard", "Pirate", "Paladin", "Baron", "Rogue", "Bard",
 			"Deer", "Horse", "Oxen"];
-		return choose(adj) +" "+ choose(obj);
+		return rng.choice(adj) +" "+ rng.choice(obj);
 	}
-	console.log("Welcome to the "+randInnName()+"! Finest tavern here!");
+
+	function RNG(seed) {
+		// LCG using GCC's constants
+		// see https://stackoverflow.com/questions/424292/seedable-javascript-random-number-generator
+		this.m = 0x80000000; // 2**31;
+		this.a = 1103515245;
+		this.c = 12345;
+		this.state = seed ? seed : Math.floor(Math.random() * (this.m-1));
+	}
+	RNG.prototype.nextInt = function() {
+		this.state = (this.a * this.state + this.c) % this.m;
+		return this.state;
+	}
+	RNG.prototype.nextFloat = function() {
+		// returns in range [0,1]
+		return this.nextInt() / (this.m - 1);
+	}
+	RNG.prototype.nextRange = function(start, end) {
+		// returns in range [start, end): including start, excluding end
+		// can't modulu nextInt because of weak randomness in lower bits
+		var rangeSize = end - start;
+		var randomUnder1 = this.nextInt() / this.m;
+		return start + Math.floor(randomUnder1 * rangeSize);
+	}
+	RNG.prototype.choice = function(array) {
+		return array[this.nextRange(0, array.length)];
+	}
 
 	startGame();
+	console.log("Welcome to the "+randInnName()+"! Finest tavern here!");
+
 })();
