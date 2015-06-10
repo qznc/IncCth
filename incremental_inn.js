fdbb21076644f37e2572df173f380a112f89eb85
Andreas Zwinkau <qznc@web.de>
2015-06-10 22:42:00 +0200

    generate random Inn names

--- incremental_inn.js
+++ incremental_inn.js
@@ -35,5 +35,63 @@
 		obj[key] = val;
 	}
 
+	function assert(condition, message) {
+		if (!condition) {
+			message = message || "Assertion failed";
+			if (typeof Error !== "undefined") {
+				throw new Error(message);
+			}
+			throw message; // Fallback
+		}
+	}
+
+	function niceNumber(n) {
+		for (i = 7; i >= 0; i--) {
+			var modfac = Math.pow(10,i);
+			if (Math.abs(n) > (2*modfac)) {
+				return n - (n % modfac);
+			}
+		}
+		return n;
+	}
+
+	function randRange(min,max) {
+		var range = max-min;
+		assert (range >= 0);
+		return min + (Math.random() * range);
+	}
+
+	function randIndex(list) {
+		return (Math.random() * (list.length-1)).toFixed(0);
+	}
+
+	function choose(items) {
+		var index = (Math.random() * (items.length-1)).toFixed(0);
+		return items[index];
+	}
+
+	function randPop(list) {
+		var idx = randIndex(list);
+		var item = list[idx];
+		list.splice(idx,1);
+		return item;
+	}
+
+	function randInnName() {
+		var adj = ["Prancing", "Dancing", "Drinking", "Jolly",
+			"Damned", "Dead", "Lost",
+			"Cold", "Hot",
+			"Red", "Green", "Blue", "Yellow", "Purpur"];
+		var obj = ["Pony", "Dragon", "Lion", "Beetle", "Honey Bee",
+			"Oak", "Walnut", "Birch", "Beech",
+			"Kobold", "Orc", "Gnoll",
+			"Rat", "Swine",
+			"Lantern",
+			"Wizard", "Pirate", "Paladin", "Baron", "Rogue", "Bard",
+			"Deer", "Horse", "Oxen"];
+		return choose(adj) +" "+ choose(obj);
+	}
+	console.log("Welcome to the "+randInnName()+"! Finest tavern here!");
+
 	startGame();
 })();
